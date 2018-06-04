7855c67831f74155a1ca6ae7d354555825bda3e6
10510 <10510@hs-90540-103.lan.summit.k12.nj.us>
2018-06-04 08:43:34 -0400

    Make textLabel

--- TitleScreenWorld.java
+++ TitleScreenWorld.java
@@ -8,7 +8,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreenWorld extends World
 {
-
     /**
      * Constructor for objects of class TitleScreenWorld.
      * 
@@ -17,5 +16,6 @@ public class TitleScreenWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        getBackground().setTransparency(100);
     }
 }
