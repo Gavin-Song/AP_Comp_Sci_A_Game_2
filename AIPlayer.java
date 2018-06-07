50035ab6d510ee7ea3afefbf833a400def12634e
11634 <11634@hs-90533-103.lan.summit.k12.nj.us>
2018-06-07 09:38:27 -0400

    Player class

---
+++ AIPlayer.java
@@ -0,0 +1,14 @@
+/**
+ * Write a description of class AIPlayer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AIPlayer extends Player 
+{
+    // instance variables - replace the example below with your own
+    public AIPlayer(Country country, int score)
+    {
+        super(false, country, score);
+    }
+}
---
+++ Player.java
@@ -0,0 +1,19 @@
+/**
+ * Write a description of class Player here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public abstract class Player  
+{
+    // instance variables - replace the example below with your own
+    private boolean isHuman;
+    private Country country;
+    private int score;
+    public Player(boolean human, Country country, int score)
+    {
+        this.isHuman = human;
+        this.country = country;
+        this.score = score;
+    }
+}
